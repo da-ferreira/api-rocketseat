8092b1a1a36cbcf2cb93d1e7168eca5b300884c9
David Ferreira <ferreiradavid361@gmail.com>
2022-06-06 16:11:53 -0300

    Route params: parametros atraves da rota

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ app.listen(3000, () => console.log('Aplicação iniciada'));
 // Middleware que transforma tudo que chega na API para Json
 app.use(express.json());
 
-app.route('/').post((req, res) => {
-	const { nome, cidade, livros } = req.body; // const nome = req.body.nome, const cidade = req.body.cidade
-	// res.send(`${nome} | ${cidade} | ${livros}`);
-	res.send(livros);
-	// console.log(`${nome} | ${cidade} | ${livros}`);
-	console.log(...livros);
+app.route('/').get((req, res) => res.send('Olá'));
+
+app.route('/:nome').get((req, res) => {
+	res.send(req.params);
+});
+
+app.route('/identidade/:nome').get((req, res) => {
+	res.send(req.params);
 });
