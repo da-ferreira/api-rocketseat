fdacb4d2502a2a463f92d16856c13c37c814374f
da-ferreira <ferreiradavid361@gmail.com>
2022-06-07 09:04:39 -0300

    eslint config + consumo de API

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
 
-app.listen(3000, () => console.log('Aplicação iniciada'));
+app.listen(3000, () => console.log('Aplicação iniciada (http://127.0.0.1:3000)'));
 
 // Middleware que transforma tudo que chega na API para Json
 app.use(express.json());
 
-app.route('/').get((req, res) => res.send(req.query));
-
-app.route('/about/user').get((req, res) => res.send(req.query));
+app.route('/')
+    .get((req, res) => {
+        axios.get('https://api.github.com/users/da-ferreira')
+        
+            // Mostra a foto de usuário do GitHub
+            .then(result => res.send(`<img src="${result.data.avatar_url}"/>`))
+            .catch(err => console.log(err));
+    });
